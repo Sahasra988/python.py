567af9ecebab10bff12758934018e0447390c074
T.Sai sahasra <thotasahasra877@gmail.com>
2026-06-22 14:48:21 +0530

    Added string practice code

---
+++ stringscon.py
@@ -0,0 +1,22 @@
+# Student Information using Strings
+
+college = "Sreenidhi University"
+student_name = "Sahasra"
+course = "Python Programming"
+
+print("College Name:", college)
+print("Student Name:", student_name)
+print("Course:", course)
+
+# String operations
+print("\nUpper Case:", student_name.upper())
+print("Lower Case:", course.lower())
+print("Length of College Name:", len(college))
+
+# First and last character
+print("First Letter:", student_name[0])
+print("Last Letter:", student_name[-1])
+
+# String Concatenation
+message = student_name + " is learning " + course
+print(message)
