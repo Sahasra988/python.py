d684b1b4c2b60a75ef5c2433f281534da3386373
T.Sai sahasra <thotasahasra877@gmail.com>
2026-06-29 22:08:12 +0530

    Added Boolean Values and Operations program

---
+++ boolean.py
@@ -0,0 +1,27 @@
+# boolean values
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+# basic boolean operations
+a = True
+b = False
+print(a and b)
+print(a or b)   
+print(not a)
+
+# example
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+# evaluting a boolean expression
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
