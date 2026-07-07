9d6637b0914869ffb92a22e6be1c378d6e8a222a
T.Sai sahasra <thotasahasra877@gmail.com>
2026-07-07 22:52:08 +0530

    Control statemnets

---
+++ control staments.py
@@ -0,0 +1,20 @@
+#1. if Statement
+ge = 20
+if age >= 18:
+    print("Eligible to vote")
+#2. if-else Statement
+age = 16
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
+#3. if-elif-else Statement
+marks = 85
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
